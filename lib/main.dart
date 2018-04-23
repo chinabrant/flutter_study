7df2412f7166def4a3dda86bd2e349677e5ce110
brant <sjwu1234@gmail.com>
2018-04-23 17:29:45 +0800

    添加下拉刷新例子

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import './refreshindicator/refreshIndicator_demo.dart';
 
 void main() => runApp(new MyApp());
 
@@ -9,32 +11,15 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'Flutter实例'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -43,67 +28,58 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // 这个key用来在不是手动下拉，而是点击某个button或其它操作时，代码直接触发下拉刷新
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      new GlobalKey<RefreshIndicatorState>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  List<Map<String, String>> list = <Map<String, String>>[];
+
+  @override
+  void initState() {
+    super.initState();
+    list.add({"title": '下拉刷新', 'type': 'pullToRefresh'});
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        // 去掉导航栏下面的阴影
+        elevation: 0.0,
         title: new Text(widget.title),
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: new ListView.builder(
+        // 这句是在list里面的内容不足一屏时，list可能会滑不动，加上就一直都可以滑动
+        physics: const AlwaysScrollableScrollPhysics(),
+        itemCount: this.list.length,
+        itemBuilder: (_, int index) => _createItem(context, list[index]),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  _createItem(BuildContext context, Map<String, String> map) {
+    return new GestureDetector(
+      onTap: () {
+        if (map['type'] == 'pullToRefresh') {
+          // 下拉刷新
+          Navigator.of(context).push(new PageRouteBuilder(pageBuilder:
+              (BuildContext context, Animation<double> animation,
+                  Animation<double> secondaryAnimation) {
+            return new RefreshIndicatorDemo();
+          }));
+        }
+      },
+      child: new Column(
+        children: <Widget>[
+          new Container(
+            height: 24.0,
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: new Text(map['title']),
+          ),
+          Divider(height: 0.5,)
+        ],
+      ),
     );
   }
 }
---
+++ lib/refreshindicator/refreshIndicator_demo.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+
+
+class RefreshIndicatorDemo extends StatefulWidget {
+  RefreshIndicatorDemo({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<RefreshIndicatorDemo> {
+
+  // 这个key用来在不是手动下拉，而是点击某个button或其它操作时，代码直接触发下拉刷新
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = new GlobalKey<
+      RefreshIndicatorState>();
+
+  List<String> list = <String>[];
+
+  @override
+  void initState() {
+    super.initState();
+
+    list.add('下拉添加新数据');
+  }
+
+  Future<Null> _getData() {
+    final Completer<Null> completer = new Completer<Null>();
+
+    // 启动一下 [Timer] 在3秒后，在list里面添加一条数据，关完成这个刷新 
+    new Timer(Duration(seconds: 3), () {
+
+      // 添加数据，更新界面
+      setState(() {
+        list.add("新加数据${list.length}");  
+      });
+
+      // 完成刷新
+      completer.complete(null);
+
+    });
+
+    return completer.future;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return new Scaffold(
+      appBar: new AppBar(
+        // 去掉导航栏下面的阴影
+        elevation: 0.0,
+        title: new Text('下拉刷新'),
+      ),
+      body: new RefreshIndicator(
+        key: _refreshIndicatorKey,
+        onRefresh: _getData,  // onRefresh 参数是一个 Future<Null> 的回调
+        child: new ListView.builder(
+          // 这句是在list里面的内容不足一屏时，list可能会滑不动，加上就一直都可以滑动
+          physics: const AlwaysScrollableScrollPhysics(), 
+          itemCount: this.list.length,
+          itemBuilder: (_, int index) => _createItem(list[index]),
+        ),
+      ),
+    );
+  }
+
+  _createItem(String title) {
+    return new Column(
+        children: <Widget>[
+          new Container(
+            height: 24.0,
+            margin: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: new Text(title),
+          ),
+          Divider(height: 0.5,)
+        ],
+      );
+  }
+}
